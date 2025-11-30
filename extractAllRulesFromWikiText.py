ce5bd0e904e35267b2a5c2f37ac5fe5cdc553ca2
lawdataANDotorita <shayz@otorita.net>
2025-11-30 13:18:26 +0200

    when we encode the html content we should state a flag that knows how to translate certain chars from one encoding to the other

--- extractAllRulesFromWikiText.py
+++ extractAllRulesFromWikiText.py
@@ -431,8 +431,8 @@ class WikiTextLinkExtractor:
                     print(f"Could not extract content from: {link_data['url']}")
                     continue
 
-                content_windows1255 = content.encode('windows-1255', errors='replace').decode('windows-1255')
-                file_hash1 = file_hash(content_windows1255)
+               content_windows1255 = content.encode('windows-1255', errors='xmlcharrefreplace').decode('windows-1255')
+               file_hash1 = file_hash(content_windows1255)
 
                 # Create filename from URL
                 parsed_url = urlparse(link_data['url'])
